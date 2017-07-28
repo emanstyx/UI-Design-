545657ae2b8529ea7cb1fe25c23dd69ff014b95a
EmanPexam <adlaone95@gmail.com>
2017-07-28 12:30:40 +0800

    dwad

--- public/js/index.js
+++ public/js/index.js
@@ -1,107 +1,43 @@
-var LoginModalController = {
-    tabsElementName: ".logmod__tabs li",
-    tabElementName: ".logmod__tab",
-    inputElementsName: ".logmod__form .input",
-    hidePasswordName: ".hide-password",
-    
-    inputElements: null,
-    tabsElement: null,
-    tabElement: null,
-    hidePassword: null,
-    
-    activeTab: null,
-    tabSelection: 0, // 0 - first, 1 - second
-    
-    findElements: function () {
-        var base = this;
-        
-        base.tabsElement = $(base.tabsElementName);
-        base.tabElement = $(base.tabElementName);
-        base.inputElements = $(base.inputElementsName);
-        base.hidePassword = $(base.hidePasswordName);
-        
-        return base;
-    },
-    
-    setState: function (state) {
-    	var base = this,
-            elem = null;
-        
-        if (!state) {
-            state = 0;
-        }
-        
-        if (base.tabsElement) {
-        	elem = $(base.tabsElement[state]);
-            elem.addClass("current");
-            $("." + elem.attr("data-tabtar")).addClass("show");
-        }
+$('.form').find('input, textarea').on('keyup blur focus', function (e) {
   
-        return base;
-    },
-    
-    getActiveTab: function () {
-        var base = this;
-        
-        base.tabsElement.each(function (i, el) {
-           if ($(el).hasClass("current")) {
-               base.activeTab = $(el);
-           }
-        });
-        
-        return base;
-    },
-   
-    addClickEvents: function () {
-    	var base = this;
-        
-        base.hidePassword.on("click", function (e) {
-            var $this = $(this),
-                $pwInput = $this.prev("input");
-            
-            if ($pwInput.attr("type") == "password") {
-                $pwInput.attr("type", "text");
-                $this.text("Hide");
-            } else {
-                $pwInput.attr("type", "password");
-                $this.text("Show");
-            }
-        });
- 
-        base.tabsElement.on("click", function (e) {
-            var targetTab = $(this).attr("data-tabtar");
-            
-            e.preventDefault();
-            base.activeTab.removeClass("current");
-            base.activeTab = $(this);
-            base.activeTab.addClass("current");
-            
-            base.tabElement.each(function (i, el) {
-                el = $(el);
-                el.removeClass("show");
-                if (el.hasClass(targetTab)) {
-                    el.addClass("show");
-                }
-            });
-        });
-        
-        base.inputElements.find("label").on("click", function (e) {
-           var $this = $(this),
-               $input = $this.next("input");
-            
-            $input.focus();
-        });
-        
-        return base;
-    },
-    
-    initialize: function () {
-        var base = this;
-        
-        base.findElements().setState().getActiveTab().addClickEvents();
-    }
-};
+  var $this = $(this),
+      label = $this.prev('label');
 
-$(document).ready(function() {
-    LoginModalController.initialize();
+	  if (e.type === 'keyup') {
+			if ($this.val() === '') {
+          label.removeClass('active highlight');
+        } else {
+          label.addClass('active highlight');
+        }
+    } else if (e.type === 'blur') {
+    	if( $this.val() === '' ) {
+    		label.removeClass('active highlight'); 
+			} else {
+		    label.removeClass('highlight');   
+			}   
+    } else if (e.type === 'focus') {
+      
+      if( $this.val() === '' ) {
+    		label.removeClass('highlight'); 
+			} 
+      else if( $this.val() !== '' ) {
+		    label.addClass('highlight');
+			}
+    }
+
+});
+
+$('.tab a').on('click', function (e) {
+  
+  e.preventDefault();
+  
+  $(this).parent().addClass('active');
+  $(this).parent().siblings().removeClass('active');
+  
+  target = $(this).attr('href');
+
+  $('.tab-content > div').not(target).hide();
+  
+  $(target).fadeIn(600);
+  
 });
